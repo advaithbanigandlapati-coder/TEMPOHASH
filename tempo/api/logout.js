cbbaf589c81cc952c6dcb1535236a0ca3ea964b7
AdviB <advaith.banigandlapati@gmail.com>
2026-05-12 17:30:59 -0400

    Delete tempo directory

--- tempo/api/logout.js
+++
@@ -1,9 +0,0 @@
-import { clearSessionCookie } from './_lib/session.js';
-
-export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'method not allowed' });
-  }
-  clearSessionCookie(res);
-  return res.status(200).json({ ok: true });
-}
